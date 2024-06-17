266a76c623953aaf4e1a99ba7e3db88bb0b6a5e7
eidynho <zviniciuseidy2@gmail.com>
2024-06-17 01:32:53 -0300

    feat: get people pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import { Inter } from "next/font/google";
 import { getPersons } from "@/services/personService";
 import type { TGetPersonsParams } from "@/services/personService.types";
 
-import { NotFoundPersonCard } from "@/components/NotFoundPersonCard";
 import { PersonCard } from "@/components/PersonCard";
 import { PersonEntity } from "@/entities/person";
 import { CreatePersonForm } from "@/components/CreatePersonForm";
@@ -12,6 +11,8 @@ import { CreatePersonForm } from "@/components/CreatePersonForm";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+    const [page, setPage] = useState(0);
+    const [paginationData, setPaginationData] = useState({ totalElements: 0, totalPages: 0 });
     const [people, setPeople] = useState<PersonEntity[]>([]);
     const [isFetchingPeople, setIsFetchingPeople] = useState(false);
 
@@ -20,12 +21,12 @@ export default function Home() {
         cpf,
         birthDateStr,
         page,
-        perPage,
+        perPage = 3,
     }: TGetPersonsParams) => {
         setIsFetchingPeople(true);
 
         try {
-            const { content } = await getPersons({
+            const { content, totalElements, totalPages } = await getPersons({
                 name,
                 cpf,
                 birthDateStr,
@@ -34,6 +35,7 @@ export default function Home() {
             });
 
             setPeople(content);
+            setPaginationData({ totalElements, totalPages });
         } catch (error) {
             console.error(error);
         } finally {
@@ -50,8 +52,8 @@ export default function Home() {
     }
 
     useEffect(() => {
-        fetchPeople({});
-    }, []);
+        fetchPeople({ page });
+    }, [page]);
 
     return (
         <main
@@ -60,31 +62,64 @@ export default function Home() {
             {isFetchingPeople ? (
                 <div>Carregando...</div>
             ) : (
-                <div>
-                    {!people.length ? (
-                        <NotFoundPersonCard addPersonIntoList={addPersonIntoList} />
-                    ) : (
-                        <div className="flex flex-col gap-4 lg:flex-row">
-                            <div className="flex flex-col gap-2">
-                                <span className="font-semibold">Criação de nova pessoa</span>
+                <div className="flex flex-col gap-4 lg:flex-row">
+                    <div className="flex flex-col gap-2">
+                        <span className="font-semibold">Criação de nova pessoa</span>
 
-                                <div className="mb-6 rounded-lg border px-8 py-6">
-                                    <CreatePersonForm addPersonIntoList={addPersonIntoList} />
+                        <div className="mb-6 rounded-lg border px-8 py-6">
+                            <CreatePersonForm addPersonIntoList={addPersonIntoList} />
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className="flex max-h-[670px] flex-col gap-2 overflow-auto">
+                            <span className="font-semibold">
+                                Lista de pessoas criadas ({paginationData.totalElements})
+                            </span>
+
+                            {!people.length ? (
+                                <div className="flex flex-col gap-2">
+                                    <div className="mx-auto w-full max-w-lg rounded-lg border px-6 py-4">
+                                        Nenhuma pessoa encontrada.
+                                    </div>
                                 </div>
-                            </div>
-
-                            <div className="flex flex-col gap-2">
-                                <span className="font-semibold">Lista de pessoas criadas</span>
-
-                                {people.map((person) => (
+                            ) : (
+                                people.map((person) => (
                                     <PersonCard
                                         person={person}
                                         removeFromPeopleList={removeFromPeopleList}
                                     />
-                                ))}
-                            </div>
+                                ))
+                            )}
                         </div>
-                    )}
+
+                        {!!people.length && (
+                            <div className="flex flex-col">
+                                <div className="mt-4 flex gap-2">
+                                    <button
+                                        className="w-1/2 rounded bg-blue-500 p-2 text-white transition duration-200 hover:bg-blue-600 disabled:bg-gray-400"
+                                        onClick={() => setPage((state) => state - 1)}
+                                        disabled={page === 0}
+                                    >
+                                        Anterior
+                                    </button>
+
+                                    <button
+                                        className="w-1/2 rounded bg-blue-500 p-2 text-white transition duration-200 hover:bg-blue-600 disabled:bg-gray-400"
+                                        onClick={() => setPage((state) => state + 1)}
+                                        disabled={paginationData.totalPages - 1 <= page}
+                                    >
+                                        Próxima
+                                    </button>
+                                </div>
+
+                                <div className="mt-2 text-center font-medium">
+                                    Pessoas por página: 3 / Página {page + 1} de{" "}
+                                    {paginationData.totalPages}
+                                </div>
+                            </div>
+                        )}
+                    </div>
                 </div>
             )}
         </main>
