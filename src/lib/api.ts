262811949edf4589ae6ee12afbe3ef105e936db9
eidynho <zviniciuseidy2@gmail.com>
2024-06-17 02:32:21 -0300

    feat: api base url based on env

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:8080";
+
 export const api = axios.create({
-    baseURL: "http://127.0.0.1:8080",
+    baseURL: baseURL,
     headers: {
         "Content-Type": "application/json",
     },
